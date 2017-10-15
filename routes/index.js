fa582a4ab7e628f9a512d67e98a20ceac0c781a5
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-10-15 11:34:42 +0200

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'SecretSanta'});
+  res.render('index', { title: 'SecretSanta', mainTitle: 'SecretSanta'});
 });
 
 // router.get('/insertUser', function(req, res){
--- routes/insertUser.js
+++ routes/insertUser.js
@@ -5,7 +5,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
-    res.render('insertUser', { title: 'insert a user'});
+    res.render('insertUser', { title: 'insert a user', mainTitle: 'SecretSanta'});
 });
+
+// Handle Author create on POST
+exports.author_create_post = function(req, res) {
+    res.send('NOT IMPLEMENTED: Author create POST');
+};
 
 module.exports = router;
