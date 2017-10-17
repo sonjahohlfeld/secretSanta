5b78269ce797e0cf5865bf0302d119d87db1ece4
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-10-17 18:15:11 +0200

    Add functionality for displaying user data

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,6 +2,27 @@
  * Created by sonja on 10/15/17.
  */
 
+var User = require('../models/user');
+var async = require('async');
+
 exports.index = function(req, res) {
-    res.render('index', { title: 'SecretSanta ', mainTitle: 'SecretSanta '});
+
+    async.parallel({
+        user_count: function(callback){
+            User.count(callback);
+        },
+        user_name: function(callback){
+            var query = User.find();
+            query.select('username email');
+            query.exec(callback);
+        }
+    }, function(err, results){
+        res.render('index', { title: 'SecretSanta ', mainTitle: 'SecretSanta ', error: err, data: results});
+    });
+
+    // User.find()
+    //     .exec(function(err, userlist){
+    //         if(err) {return next(err);}
+    //         res.render('index', { title: 'SecretSanta ', mainTitle: 'SecretSanta ', user_name: userlist})
+    //     })
 };
