6577e3c41957b117a2ef0970b774b06fe0e663d4
Sonja Hohlfeld <development@sonja-hohlfeld.de>
2017-10-25 14:06:52 +0200

    Update routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-var user_controller = require('../controllers/userController');
-var index_controller = require('../controllers/indexController');
+var index_controller = require('../controllers/index');
 
 router.get('/', index_controller.index);
 
-router.get('/insertUser', user_controller.user_insert_get);
-
-router.post('/insertUser', user_controller.user_insert_post);
+router.post('/insertUser', index_controller.insert_user_post);
 
 module.exports = router;
